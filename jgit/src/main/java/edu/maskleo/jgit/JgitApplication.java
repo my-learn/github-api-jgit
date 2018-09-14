799b6c6247bcf9473ff448603a778bade06bb98d
test <test@example.com>
2018-09-14 09:21:12 +0800

    x

--- jgit/src/main/java/edu/maskleo/jgit/JgitApplication.java
+++ jgit/src/main/java/edu/maskleo/jgit/JgitApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JgitApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         SpringApplication.run(JgitApplication.class, args);
     }
 }
--- jgit/src/main/java/edu/maskleo/jgit/JgitTest.java
+++ jgit/src/main/java/edu/maskleo/jgit/JgitTest.java
@@ -9,22 +9,31 @@ import java.io.RandomAccessFile;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Random;
 
 public class JgitTest {
 
     public static void main(String[] args) {
         try {
-            for (int j = -1365; j < 0; j++) {
-                Calendar c = Calendar.getInstance();
-                c.add(Calendar.DAY_OF_MONTH, j);
-                editText();
-                Repository rep = new FileRepository("E:\\learing_code\\github-api-jgit\\.git");
-                Git git = new Git(rep);
-                PersonIdent defaultCommitter = new PersonIdent(git.getRepository());
-                PersonIdent committer = new PersonIdent(defaultCommitter, c.getTime());
-                git.commit().setMessage("auto commit").setCommitter(committer).call();
-                System.out.println(j);
+            for (int j = -365; j < 0; j++) {
+                if (!isR()) {
+                    continue;
+                }
+                java.util.Random r = new Random();
+                int kd = r.nextInt(100);
+                for(int k = 0;k<kd;k++){
+                    Calendar c = Calendar.getInstance();
+                    c.add(Calendar.DAY_OF_MONTH, j);
+                    editText();
+                    Repository rep = new FileRepository("E:\\learing_code\\github-api-jgit\\.git");
+                    Git git = new Git(rep);
+                    PersonIdent defaultCommitter = new PersonIdent(git.getRepository());
+                    PersonIdent committer = new PersonIdent(defaultCommitter, c.getTime());
+                    git.commit().setMessage("auto commit").setCommitter(committer).call();
+                    System.out.println(j);
+                }
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -32,7 +41,7 @@ public class JgitTest {
     }
 
     private static void editText() throws Exception {
-        String path = "E:\\learing_code\\github-api-jgit\\2018070313.txt";
+        String path = "E:\\learing_code\\github-api-jgit\\2018070317.txt";
         RandomAccessFile raf = new RandomAccessFile(path, "rw");
         raf.writeBytes("auto commit" + System.nanoTime());
         raf.close();
@@ -40,5 +49,11 @@ public class JgitTest {
 
     private static String buildMessage() {
         return String.format("Update By JGit At %s", new SimpleDateFormat().format(new Date()));
+    }
+
+    private static boolean isR() {
+        java.util.Random r = new Random();
+        int i = r.nextInt(7);
+        return i == 0;
     }
 }
